f14f987807b1c746e4bd0d867076f7c581e601fe
CZG0712 <2892537581@qq.com>
2022-08-10 10:36:09 +0800

    project init

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+// 在这个文件里封装请求
+// 以下为示例
+function getMoleAndSequence() {
+  return request({
+    loading: true, //是否显示加载效果
+    url: "/panda/info/getHeader",
+    method: "GET",
+  });
+}
+
+export { getMoleAndSequence };
