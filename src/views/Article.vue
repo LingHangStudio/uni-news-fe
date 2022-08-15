bd782d7963610805a11340f294e1b4de6618dd3d
Monkeyhbd <monkeyhbd@163.com>
2022-08-16 01:02:58 +0800

    更新了文章详情页面
    遍历文章正文部分 DOM 树，删除所有节点元素内联样式，然后统一用外部样式
    控制样式，从而得到风格统一的文章详情界面。解决了原来存在内联样式干扰而
    造成的风格不统一问题。i

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>文章 {{articleObj.title}}</div>
+  <div v-bind:key="$route.fullPath">
+    <div class="article-wrapper">
+      <div class="article">
+        <div class="article-title">
+          <h1>{{articleObj.title}}</h1>
+        </div>
+        <div class="article-time">{{articleObj.time}}</div>
+        <div id="content" class="article-content content" v-html="articleObj.text"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -14,10 +24,77 @@ export default {
     }
   },
 
+  methods: {
+    ficImgSrc: function(contentElement, piclist) {
+      var imgs = contentElement.getElementsByTagName('img')
+      var len = imgs.length < piclist.length ? imgs.length : piclist.length
+      for (var idx = 0; idx < len; idx += 1) {
+        imgs[idx]['src'] = piclist[idx]
+      }
+    },
+
+    clearInlineStyle: function(contentElement) {
+      function clearInlineStyleCore(contentElement) {
+        var children = contentElement.children
+        if (children != undefined && children.length > 0) {
+          for (var idx = 0; idx < children.length; idx += 1) {
+            console.log(children[idx])
+            children[idx].style = undefined
+            clearInlineStyleCore(children[idx])
+          }
+        }
+        else {
+          return null
+        }
+      }
+      clearInlineStyleCore(contentElement)
+    }
+  },
+
   mounted: function() {
-    console.log(ArticleStore[this.$route.params.id].title)
     console.log(this.$route.params.id)
+    console.log(ArticleStore[this.$route.params.id])
     this.articleObj = ArticleStore[this.$route.params.id]
+    var originPiclist = this.articleObj.piclist
+    this.$nextTick(function () {
+      // Code that will run only after the
+      // entire view has been rendered
+      var piclist = []
+      for (var idx = 0; idx < originPiclist.length; idx += 1) {
+        piclist.push(originPiclist[idx].pic)
+      }
+      console.log(piclist)
+      var contentElement = document.getElementById('content')
+      this.ficImgSrc(contentElement, piclist)
+      this.clearInlineStyle(contentElement)
+    })
   }
 }
 </script>
+
+<style>
+@import '@/assets/css/markdown.css';
+
+.article-wrapper {
+  padding: 16px 0 0 0;
+}
+
+.article {
+  padding: 0 16px;
+  color: #333333;
+}
+
+.article-title h1 {
+  margin: 0;
+  padding: 0;
+  font-size: 22px;
+  line-height: 32px;
+  margin-bottom: 12px;
+}
+
+.article-time {
+  font-size: 13px;
+  color: #999999;
+  margin-bottom: 32px;
+}
+</style>
