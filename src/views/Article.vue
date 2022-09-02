203664521c751eec47ed4d0ffb1da43198f483f6
Monkeyhbd <monkeyhbd@163.com>
2022-09-02 20:48:55 +0800

    文章详情界面新增了访问原文扩展栏

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -15,6 +15,17 @@
         </div>
         <div class="article-time">{{articleObj.time}}</div>
         <div id="content" class="article-content content" v-html="articleObj.text"></div>
+        <div class="article-link-container">
+          <div class="article-link-bar">
+            <div class="article-link-bar-title">原文</div>
+            <div class="article-link-bar-text">https://www.monkeyhbd.com</div>
+            <div class="article-link-bar-widgets">
+              <div class="article-link-bar-widget-copy-text" v-on:click="copyHrefToClipboard()">
+                <img v-bind:src="require('@/assets/icon/icon-copy-efefef.svg')">
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -84,6 +95,20 @@ export default {
       }
       removeBlankLineCore(contentElement)
     },
+
+    copyToClipboard: function(text) {
+      var tmp = document.createElement('input')
+      tmp.style = 'position: fixed; z-index: -100;'
+      document.body.appendChild(tmp)
+      tmp.value = text
+      tmp.select()
+      document.execCommand('copy')
+      document.body.removeChild(tmp)
+    },
+
+    copyHrefToClipboard: function() {
+      this.copyToClipboard(document.getElementsByClassName('article-link-bar-text')[0].innerHTML)
+    }
   },
 
   mounted: function() {
@@ -183,5 +208,65 @@ export default {
   font-size: 13px;
   color: #999999;
   margin-bottom: 24px;
+}
+
+:root {
+  --x-bar-line-height: 24px;
+}
+
+.article-link-container {
+  margin-top: 24px;
+  background: linear-gradient(to right, rgb(0, 50, 255), rgb(185, 199, 255));
+  padding: 4px 0;
+  border-radius: 8px;
+}
+
+.article-link-bar {
+  display: flex;
+  padding: 0 8px;
+  height: var(--x-bar-line-height);
+  line-height: var(--x-bar-line-height);
+  font-size: 14px;
+}
+
+.article-link-bar-title {
+  flex-grow: 0;
+  flex-shrink: 0;
+  padding-left: 2px;
+  padding-right: 10px;
+  color: #ffffff;
+  font-size: 13px;
+}
+
+.article-link-bar-text {
+  flex-grow: 1;
+  flex-shrink: 1;
+  overflow: hidden;
+  background-color: rgba(255, 255, 255, 0.7);
+  border-radius: 4px;
+  padding: 0 8px;
+}
+
+.article-link-bar-widgets {
+  flex-grow: 0;
+  flex-shrink: 0;
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+}
+
+.article-link-bar-widget-copy-text {
+  width: calc(var(--x-bar-line-height) - 2px);
+  height: calc(var(--x-bar-line-height) - 2px);
+  border-radius: 50%;
+  background-color: rgb(0, 50, 255);
+  display: flex;
+  box-sizing: border-box;
+  padding: 6px;
+}
+
+.article-link-bar-widget-copy-text img {
+  width: 100%;
+  height: 100%;
 }
 </style>
