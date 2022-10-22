82b7c388eaea43ec6c3b7fc85f6a2ee189ee1f5b
Monkeyhbd <monkeyhbd@163.com>
2022-10-22 10:56:41 +0800

    新增原文扩展卡片 OriginArticleX.
    将原来集成在文章详情界面的原文扩展卡片提取成独立的组件。

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -15,19 +15,10 @@
         </div>
         <div class="article-time">发布时间：{{articleObj.dateStr}}</div>
         <div id="content" class="article-content content" v-html="articleObj.content"></div>
-        <div class="article-link-container">
-          <div class="article-link-bar">
-            <div class="article-link-bar-title">原文</div>
-            <div class="article-link-bar-text">
-              <span>{{articleObj.href}}</span>
-            </div>
-            <div class="article-link-bar-widgets">
-              <div class="article-link-bar-widget-copy-text" v-on:click="copyHrefToClipboard()">
-                <img v-bind:src="require('@/assets/icon/icon-copy-efefef.svg')">
-              </div>
-            </div>
-          </div>
-        </div>
+        <OriginArticleX class="origin-article-x"
+          v-bind:origin-href="articleObj.href"
+          v-on:show-message-box="showMessageBox">
+        </OriginArticleX>
       </div>
     </div>
     <div id="message-box" class="message-box close">
@@ -37,11 +28,16 @@
 </template>
 
 <script>
+import OriginArticleX from '@/components/ArticleXCard/OriginArticleX.vue'
 import ArticleStore from '@/store/ArticleStore'
 import newsApi from '@/api/newsApi'
 
 export default {
   name: 'Article',
+
+  components: {
+    OriginArticleX: OriginArticleX
+  },
 
   data: function() {
     return {
@@ -300,66 +296,8 @@ export default {
   --x-bar-line-height: 30px;
 }
 
-.article-link-container {
+.origin-article-x {
   margin-top: 32px;
-  background: linear-gradient(to right, rgb(0, 50, 255), rgb(185, 199, 255));
-  padding: 4px 0;
-  border-radius: 8px;
-}
-
-.article-link-bar {
-  display: flex;
-  padding: 0 10px;
-  height: var(--x-bar-line-height);
-  line-height: var(--x-bar-line-height);
-  font-size: 14px;
-}
-
-.article-link-bar-title {
-  flex-grow: 0;
-  flex-shrink: 0;
-  padding-left: 2px;
-  padding-right: 10px;
-  color: #ffffff;
-  font-size: 13px;
-}
-
-.article-link-bar-text {
-  flex-grow: 1;
-  flex-shrink: 1;
-  overflow-x: auto;
-  overflow-y: hidden;
-  background-color: rgba(255, 255, 255, 0.7);
-  border-radius: 4px;
-}
-
-.article-link-bar-text span {
-  display: inline-block;
-  padding: 0 8px;
-  width: max-content;
-}
-
-.article-link-bar-widgets {
-  flex-grow: 0;
-  flex-shrink: 0;
-  display: flex;
-  align-items: center;
-  padding-left: 10px;
-}
-
-.article-link-bar-widget-copy-text {
-  width: calc(var(--x-bar-line-height) - 2px);
-  height: calc(var(--x-bar-line-height) - 2px);
-  border-radius: 50%;
-  background-color: rgb(0, 50, 255);
-  display: flex;
-  box-sizing: border-box;
-  padding: 7px;
-}
-
-.article-link-bar-widget-copy-text img {
-  width: 100%;
-  height: 100%;
 }
 
 .message-box {
