acfe50ac10aa0fc4e806b3d7514792e7889b3bee
lkyyyy123456 <14228356+lkyyyy123456@user.noreply.gitee.com>
2025-01-26 16:57:19 +0800

    feat:利用v-if修复文章渲染问题

--- src/views/ArticleSite.vue
+++ src/views/ArticleSite.vue
@@ -41,8 +41,8 @@ console.error('Error:', error)
 };
 
 
-watch(() => route.params.id, async (newId, oldId) => {
-  if (newId !== oldId) {
+watch(() => route.params.id, async (newId) => {
+  if (newId) {
     id.value = newId;  // 更新 id
     await loadArticle(id.value)
   }
@@ -164,8 +164,10 @@ const strDate = (year, month, day) => {
 }
 
 const goBack = () => {
-  router.back()
+  router.go(-1)
 }
+
+
 
 const copyToClipboard = async (text) => {
   try {
@@ -180,7 +182,7 @@ const copyToClipboard = async (text) => {
 </script>
 
 <template>
-  <div class="article-page" :key="route.params.id">
+  <div class="article-page" v-if="route.name=='article'" :key="route.params.id">
     <div class="article-head-bar">
       <div class="article-head-bar-back-button" @click="goBack">
         <div class="article-head-bar-back-button-inner">
